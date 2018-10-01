a1f5b3aec669b1e862c4b7ec3381a354f20ecc90
tom miller <thomas.j.c.miller@outlook.com>
2018-10-01 13:38:17 +0100

    fizzbuzz refactored

---
+++ fizzbuzz_refactored.js
@@ -0,0 +1,13 @@
+function fizzbuzz(num) {
+switch (num) {
+  case (num % 3 == 0):
+  return (fizz)
+  break;
+  case (num % 5 == 0):
+  return (buzz)
+  break;
+  case (num % 3 == 0 && num % 5 == 0)
+  return (fizzbuzz)
+  }
+}
+fizzbuzz(100)
